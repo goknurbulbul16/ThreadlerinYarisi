303a77346e77399efcc088490b7d3f73cde4706d
göknur <goknur.bulbul1166@gmail.com>
2023-01-17 08:07:10 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            array.add(i);
+        }
+
+        ThreadOperations thr = new ThreadOperations(array);
+
+        execute.execute(thr);
+
+
+    }
+}
